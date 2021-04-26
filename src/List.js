8c54e7d3cd78f96ac56c58168095cae0a5f7ef3a
Vardhu2706 <ayyagari.vardhu@gmail.com>
2021-04-26 09:52:11 +0530

    Initial Project Setup.

---
+++ src/List.js
@@ -0,0 +1,16 @@
+// Importing Helpers
+import React from "react";
+
+// Importing Components/Files
+
+// Functional Component
+const List = () => {
+  return (
+    <>
+      <h2>List Component</h2>
+    </>
+  );
+};
+
+// Default Export
+export default List;
