945c30515234bbf162274e0b3873d6028e659493
Vardhu2706 <ayyagari.vardhu@gmail.com>
2021-04-26 10:31:10 +0530

    Added list & button functionality.

--- src/List.js
+++ src/List.js
@@ -4,10 +4,21 @@ import React from "react";
 // Importing Components/Files
 
 // Functional Component
-const List = () => {
+const List = ({ people }) => {
   return (
     <>
-      <h2>List Component</h2>
+      {people.map((person) => {
+        const { id, name, age, image } = person;
+        return (
+          <article key={id} className="person">
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <p>{age}</p>
+            </div>
+          </article>
+        );
+      })}
     </>
   );
 };
